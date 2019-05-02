e7d7c8b4188fc8ce7addf8c3c4817e06cbf17a8e
Vladimir Fomene <vladimirfomene@gmail.com>
2019-05-02 10:21:29 +0100

    create User model and generate its migration

---
+++ migrations/20190501201709-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      userId: {
+        allowNull: false,
+        primaryKey: true,
+				type: Sequelize.UUID,
+				defaultValue: Sequelize.literal('uuid_generate_v4()')
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      telephone: {
+        type: Sequelize.STRING
+			},
+			status: {
+				type: Sequelize.ENUM,
+				values: ['active', 'pending']
+			},
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
