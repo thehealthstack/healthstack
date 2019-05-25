76de93eb20afdb8c9e5556aaa6f500cfe9608434
Vladimir Fomene <vladimirfomene@gmail.com>
2019-05-25 17:33:26 +0100

    add aws configuration to the application

---
+++ config/aws-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	centrePasteurYaoundeBucket: "healthstack-centrepasteur-yaounde",
+	accessKeyId: "AKIAJ2TT5QAMZVG7BNVA",
+	secretAccessKey: "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"
+};
