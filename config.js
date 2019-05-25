33e86aed5f95472a38427e88f6aca749238ba05d
Vladimir Fomene <vladimirfomene@gmail.com>
2019-05-25 17:32:48 +0100

    use dotenv to load env variables

---
+++ config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+	port: process.env.PORT,
+	appEnv: process.env.NODE_ENV,
+	centrePasteurYaoundeBucket: process.env.CENTRE_PASTEUR_YAOUNDE_BUCKET,
+	awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
