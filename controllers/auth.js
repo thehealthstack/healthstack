9e68d56832892e88bfba0403b52edf58818a1151
Vladimir Fomene <vladimirfomene@gmail.com>
2019-05-25 17:41:21 +0100

    create boilerplate for app authentication

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authRouter = express.Router();
+
+
+authRouter.post('/login', (req, res, next) => {
+
+});
+
+
+authRouter.post('/logout', (req, res, next) => {
+
+});
