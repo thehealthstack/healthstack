d2f205d1e4b1ce900bfae361b2099a47767cdef6
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-04 13:40:25 +0100

    add sessionSecret, AWS region and email service source address

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ module.exports = {
 	appEnv: process.env.NODE_ENV,
 	centrePasteurYaoundeBucket: process.env.CENTRE_PASTEUR_YAOUNDE_BUCKET,
 	awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	sessionSecret: process.env.SESSION_SECRET,
+	region: process.env.REGION,
+	senderEmail: process.env.SENDER_EMAIL
 }
