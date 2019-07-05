7e5fc1f5c7b483fb9275c9842817e00c2b536d40
Vladimir Fomene <vladimirfomene@gmail.com>
2019-07-05 17:36:20 +0100

    add the return statement before every response and refactor the validation code in organizations controller

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -1,41 +1,37 @@
-const express = require('express');
-const { check, validationResult } = require('express-validator/check');
-const organizationRouter = express.Router();
 const { organization } = require('../models');
+const { validationResult } = require('express-validator/check');
 
 
-organizationRouter.get('/organizations/:id', (req, res, next) => {
+exports.getOrganizationById = (req, res, next) => {
 	organization.findOne({ where: { organizationId: req.params.id }})
 	.then(resp => {
-		if(resp.length === 0) res.status(404).json({err: 'Not Found'});
-		res.status(200).json(resp);
+		if(resp.length === 0) return res.status(404).json({err: 'Not Found'});
+		return res.status(200).json(resp);
 	})
 	.catch(err => {
 		err.status = 500;
 		err.msg = 'Query for organization failed';
-		next(err);
+		return next(err);
 	})
-});
+};
 
-organizationRouter.get('/organizations', (req, res, next) => {
+exports.getOrganizations = (req, res, next) => {
 	organization.findAll()
 	.then(resp => {
-		if(resp.length === 0) res.status(404).json({err: 'Not Found'})
-		res.status(200).json(resp);
+		if(resp.length === 0) return res.status(404).json({err: 'Not Found'})
+		return res.status(200).json(resp);
 	}).catch(err => {
 		err.status = 500;
 		err.msg = 'Query for organizations failed';
-		next(err);
+		return next(err);
 	});
-});
+};
 
-organizationRouter.post('/organizations', [
-	check('telephone').matches(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/,'g').withMessage('Telephone number is not valid'),
-	check('email').isEmail().withMessage('Email format not valid')
-], (req, res, next) => {
+
+exports.createOrganization = (req, res, next) => {
 	const errors = validationResult(req);
-  	if (!errors.isEmpty()) {
-    	return res.status(400).json({ errors: errors.array() });
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
 	}
 
 	organization.create({
@@ -50,45 +46,41 @@ organizationRouter.post('/organizations', [
 		updatedAt: new Date()
 	})
 	.then(resp => {
-		res.status(200).json(resp);
+		return res.status(200).json(resp);
 	})
 	.catch(err => {
 		err.status = 500;
 		err.msg = 'Organization creation failed'
-		next(err);
+		return next(err);
 	});
-});
+};
 
-organizationRouter.put('/organizations/:id', [
-	check('telephone').optional().matches(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/,'g').withMessage('Telephone number is not valid'),
-	check('email').optional().isEmail().withMessage('Email format not valid')
-], (req, res, next) => {
+
+exports.updateOrganization = (req, res, next) => {
 	const errors = validationResult(req);
-  	if (!errors.isEmpty()) {
-    	return res.status(400).json({ errors: errors.array() });
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
 	}
-	
+
 	organization.update(req.body, { returning: true, where: { organizationId: req.params.id }})
 	.then(resp => {
-		res.status(200).json(resp[1]);
+		return res.status(200).json(resp[1]);
 	})
 	.catch(err => {
 		err.status = 500;
 		err.msg = 'Organization update failed';
-		next(err);
+		return next(err);
 	});
-});
+};
 
-organizationRouter.delete('/organizations/:id', (req, res, next) => {
+exports.deleteOrganizationById = (req, res, next) => {
 	organization.destroy({ where: { organizationId: req.params.id }})
 	.then(resp => {
-		res.status(200).json({ msg: 'Organization deletion was successful'});
+		return res.status(200).json({ msg: 'Organization deletion was successful'});
 	})
 	.catch(err => {
 		err.status = 500;
 		err.msg = 'Organization deletion failed';
-		res.status(500).json(err);
+		return next(err);
 	})
-});
-
-exports.organizationRouter = organizationRouter;
+};
