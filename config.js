73707dfef83c1fa0904ce561b05df370fad813c6
Vladimir Fomene <vladimirfomene@gmail.com>
2019-07-05 20:13:43 +0100

    add settings for africastalking bulksms service

--- config.js
+++ config.js
@@ -8,5 +8,7 @@ module.exports = {
 	awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	sessionSecret: process.env.SESSION_SECRET,
 	region: process.env.REGION,
-	senderEmail: process.env.SENDER_EMAIL
+	senderEmail: process.env.SENDER_EMAIL,
+	africasTalkingApiKey: process.env.AFRICAS_TALKING_API_KEY,
+	africasTalkingApiUsername: process.env.AFRICAS_TALKING_USERNAME
 }
