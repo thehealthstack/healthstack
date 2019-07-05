2231ac15072e640ce5ca395bf91701103be91370
Vladimir Fomene <vladimirfomene@gmail.com>
2019-07-05 17:49:18 +0100

    add the return statement before every response and refactor the validation code in transactions controller

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,128 +1,152 @@
-const express = require('express');
-const { transaction, medicalresult, patient } = require('../models');
-const { check, validationResult } = require("express-validator/check");
-const smsService = require('../services/smsService');
-const emailService = require('../services/emailService');
-const transactionRouter = express.Router();
+const { transaction, medicalresult, patient, user } = require("../models");
+const smsService = require("../services/smsService");
+const emailService = require("../services/emailService");
+const { validationResult } = require("express-validator/check");
 
+exports.getTransactions = (req, res, next) => {
+  if (!req.query) {
+		console.log("Processing transaction without query");
+    transaction
+      .findAll({
+        include: [
+          {
+            model: medicalresult,
+            include: [{ model: patient, include: [user] }]
+          }
+        ]
+      })
+      .then(resp => {
+        if (resp.length === 0)
+          return res.status(404).json({ msg: "Not Found" });
+        return res.status(200).json(resp);
+      })
+      .catch(err => {
+        err.status = 500;
+        err.msg = "Query for transactions failed";
+        return next(err);
+      });
+  } else {
+    if (req.query.email) {
+      transaction
+        .findAll({
+          include: [medicalresult, patient],
+          where: { email: req.query.email }
+        })
+        .then(resp => {
+          if (resp.length === 0) res.status(404).send({ msg: "Not Found" });
+          return res.status(200).json(resp);
+        })
+        .catch(err => {
+          err.status = 500;
+          err.msg = "Query for transactions failed";
+          return next(err);
+        });
+    }
 
+    if (req.query.telephone) {
+      transaction
+        .findAll({
+          include: [medicalresult, patient],
+          where: { telephone: req.query.telephone }
+        })
+        .then(resp => {
+          if (resp.length === 0)
+            return res.status(404).send({ msg: "Not Found" });
+          return res.status(200).json(resp);
+        })
+        .catch(err => {
+          err.status = 500;
+          err.msg = "Query for transactions failed";
+          return next(err);
+        });
+    }
+  }
+};
 
-transactionRouter.get('/transactions', (req, res, next) => {
+exports.getTransactionById = (req, res, next) => {
+  transaction
+    .findOne({ where: { transactionId: req.params.id } })
+    .then(resp => {
+      if (resp.length === 0) return res.status(404).send({ msg: "Not Found" });
+      return res.status(200).json(resp);
+    })
+    .catch(err => {
+      err.status = 500;
+      err.msg = "Query for transactions failed";
+      return next(err);
+    });
+};
 
-	if(!req.query){
-		transaction.findAll({ include: [ medicalresult, patient ]})
-		.then(resp => {
-			if(resp.length === 0) res.status(404).json({ msg: 'Not Found'});
-			res.status(200).json(resp);
-		})
-		.catch(err => {
-			err.status = 500;
-			err.msg = 'Query for transactions failed';
-			next(err);
-		});
-	}else{
-		if(req.query.email){
-			transaction.findAll({ include: [ medicalresult, patient ], where: { email: req.query.email }})
-			.then(resp => {
-				if(resp.length === 0) res.status(404).send({ msg: 'Not Found'});
-				res.status(200).json(resp);
-			})
-			.catch(err => {
-				err.status = 500;
-				err.msg = 'Query for transactions failed'
-			});
-		}
+exports.deleteTransactionById = (req, res, next) => {
+  transaction
+    .destroy({ where: { transactionId: req.params.id } })
+    .then(resp => {
+      return res.status(200).json({ msg: "Your delete was successfull" });
+    })
+    .catch(err => {
+      err.status = 500;
+      err.msg = "Transaction delete failed";
+      return next(err);
+    });
+};
 
-		if(req.query.telephone){
-			transaction.findAll({ include: [ medicalresult, patient ], where: { telephone: req.query.telephone }})
-			.then(resp => {
-				if(resp.length === 0) res.status(404).send({ msg: 'Not Found'});
-				res.status(200).json(resp);
-			})
-			.catch(err => {
-				err.status = 500;
-				err.msg = 'Query for transactions failed'
-			});
-		}
-		
-	}
-	
-});
+exports.updateTransaction = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-transactionRouter.get('/transactions/:id', (req, res, next) => {
-	transaction.findOne({ where: { transactionId: req.params.id }})
-	.then(resp => {
-		if(resp.length === 0) res.status(404).send({ msg: 'Not Found'});
-		res.status(200).json(resp);
-	})
-	.catch(err => {
-		err.status = 500;
-		err.msg = 'Query for transactions failed';
-	});
-});
+  transaction
+    .update(req.body, {
+      returning: true,
+      where: { transactionId: req.params.id }
+    })
+    .then(resp => {
+      return res.status(200).json(resp[1]);
+    })
+    .catch(err => {
+      err.status = 500;
+      err.msg = "Transaction update failed";
+      return next(err);
+    });
+};
 
-transactionRouter.delete('/transactions/:id', (req, res, next) => {
-	transaction.destroy({ where: { transactionId: req.params.id }})
-	.then(resp => {
-		res.status(200).json({ msg: 'Your delete was successfull'});
-	})
-	.catch(err => {
-		err.status = 500;
-		err.msg = 'Transaction delete failed';
-	});
-});
-
-transactionRouter.put('/transactions/:id', [
-	check('emailTransactionStatus').optional().isIn(['accepted', 'success', 'failed']).withMessage("Email Transaction status is suppose to be either created, accepted, success or failed"),
-	check('smsTransactionStatus').optional().isIn(['accepted', 'success', 'failed']).withMessage("Sms Transaction status is suppose to be either created, accepted, success or failed")
-], (req, res, next) => {
-	const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-	}
-	
-	transaction.update(req.body, { returning: true, where: { transactionId: req.params.id }})
-	.then(resp => {
-		res.status(200).json(resp[1]);
-	})
-	.catch(err => {
-		err.status = 500;
-		err.msg = 'Transaction update failed';
-		next(err);
-	});
-});
-
-transactionRouter.post('/transactions', (req, res, next) => {
-	let smsText = smsService.generateSmsText(req.session.user.admin.organization.name, req.body.resultFiles);
-		Promise.all([
-			smsService.publishSMS(req.body.recipientPhoneNumber, smsText),
-			emailService.createSendEmailPromise(req.body.recipientEmailAddresses, { results: req.body.resultFiles.join(), lab: req.session.user.admin.organization.name })
-		])
-		.then(values => {
-			console.log(values);
-			transaction.create({
-				emailTransactionStatus: 'accepted',
-				smsTransactionStatus: 'accepted',
-				patientId: req.body.patientId,
-				medicalResultId: req.body.medicalResultId,
-				adminId: req.session.user.admin.adminId,
-				createdAt: new Date(),
-				updatedAt: new Date()
-			})
-			.then(resp => {
-				res.status(200).json(resp);
-			})
-			.catch(err => {
-				err.status = 500;
-				err.msg = 'Transaction creation failed'
-				next(err);
-			});
-		})
-		.catch(err => {
-			err.status = 500;
-			err.msg = 'Sms and Email push failed';
-			next(err);
-		})
-});
-
-exports.transactionRouter = transactionRouter;
+exports.createTransaction = (req, res, next) => {
+  let smsText = smsService.generateSmsText(
+    req.session.user.admin.organization.name,
+    req.body.resultFiles
+  );
+  Promise.all([
+    smsService.publishSMS(req.body.recipientPhoneNumber, smsText, smsService.setSmsParams),
+    emailService.createSendEmailPromise(req.body.recipientEmailAddresses, {
+      results: req.body.resultFiles.join(", "),
+      lab: req.session.user.admin.organization.name
+    })
+  ])
+    .then(values => {
+      console.log(values);
+      transaction
+        .create({
+          emailTransactionStatus: "accepted",
+          smsTransactionStatus: "accepted",
+          patientId: req.body.patientId,
+          medicalResultId: req.body.medicalResultId,
+          adminId: req.session.user.admin.adminId,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+        .then(resp => {
+          return res.status(200).json(resp);
+        })
+        .catch(err => {
+          err.status = 500;
+          err.msg = "Transaction creation failed";
+          return next(err);
+        });
+    })
+    .catch(err => {
+      err.status = 500;
+      err.msg = "Sms and Email push failed";
+      return next(err);
+    });
+};
