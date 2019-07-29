c722dc3e8ed15e0dcfea3d87db96af095f9725b7
vladimirfomene <vladimirfomene@gmail.com>
2019-07-29 11:31:16 +0100

    add redis cred en vars

--- config.js
+++ config.js
@@ -10,5 +10,8 @@ module.exports = {
 	region: process.env.REGION,
 	senderEmail: process.env.SENDER_EMAIL,
 	africasTalkingApiKey: process.env.AFRICAS_TALKING_API_KEY,
-	africasTalkingApiUsername: process.env.AFRICAS_TALKING_USERNAME
+	africasTalkingApiUsername: process.env.AFRICAS_TALKING_USERNAME,
+	redisPort: process.env.REDIS_PORT,
+	redisPassword: process.env.REDIS_PASSWORD,
+	redisHost: process.env.REDIS_HOST
 }
