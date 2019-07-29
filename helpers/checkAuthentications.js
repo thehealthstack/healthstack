1276253543bb7ff3a551026450c49d99195a72c4
vladimirfomene <vladimirfomene@gmail.com>
2019-07-29 11:33:36 +0100

    remove check for session presence

--- helpers/checkAuthentications.js
+++ helpers/checkAuthentications.js
@@ -1,38 +1,32 @@
 
 exports.isLabAgent = (req, res, next) => {
-	if(req.session){
-		if(req.session.user){
-			if(req.session.user.role === 'lab agent'){
-				return next()
-			}else{
-				return res.status(401).json({ msg: "Not Authorized"});
-			}
+	if(req.session.user){
+		if(req.session.user.role === 'lab agent'){
+			return next()
+		}else{
+			return res.status(401).json({ msg: "Not Authorized"});
 		}
 	}
 	return res.status(401).json({ msg: "Not Authorized"});
 };
 
 exports.isAdmin = (req, res, next) => {
-	if(req.session){
-		if(req.session.user){
-			if(req.session.user.role === 'healthstack'){
-				return next()
-			}else{
-				return res.status(401).json({ msg: "Not Authorized"});
-			}
+	if(req.session.user){
+		if(req.session.user.role === 'healthstack'){
+			return next()
+		}else{
+			return res.status(401).json({ msg: "Not Authorized"});
 		}
 	}
 	return res.status(401).json({ msg: "Not Authorized"});
 };
 
 exports.isAdminOrLabAgent = (req, res, next) => {
-	if(req.session){
-		if(req.session.user){
-			if(req.session.user.role === 'healthstack' || req.session.user.role === 'lab agent'){
-				return next()
-			}else{
-				return res.status(401).json({ msg: "Not Authorized"});
-			}
+	if(req.session.user){
+		if(req.session.user.role === 'healthstack' || req.session.user.role === 'lab agent'){
+			return next()
+		}else{
+			return res.status(401).json({ msg: "Not Authorized"});
 		}
 	}
 	return res.status(401).json({ msg: "Not Authorized"});
